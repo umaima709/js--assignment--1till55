29d3c7c9b19b34d9c248f9a51381c6d555bead88
umaima <113837633+umaima709@users.noreply.github.com>
2023-09-26 19:31:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// function messag (){
+//     alert("Hello")
+// }
+
+// var status= prompt("Enter your marital status");
+// if(status=="married"){
+//     console.log("not available");
+// }
+// else{
+//     console.log("available")
+// };
